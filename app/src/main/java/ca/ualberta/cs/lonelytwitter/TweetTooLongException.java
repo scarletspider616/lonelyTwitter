192ccf4c7c3ca9cef52f1adb2adb5d6dbf2b0fee
scarletspider616 <j.fallone@ualberta.ca>
2016-09-20 17:43:58 -0600

    tstingø

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
@@ -1,14 +1,8 @@
 package ca.ualberta.cs.lonelytwitter;
 
 /**
-<<<<<<< HEAD
  * Created by jm on 2016-09-13.
  */
-public class TweetTooLongException extends Exception{
-
-=======
- * Created by watts1 on 9/13/16.
- */
 public class TweetTooLongException extends Exception {
->>>>>>> 3ca5beaa9c5f5299f28bffe00b82736fc9a9647d
+
 }
