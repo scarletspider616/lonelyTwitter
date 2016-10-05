34d08bd82419f7a6c76723659a7cfa35641c66a8
scarletspider616 <j.fallone@ualberta.ca>
2016-10-04 18:25:57 -0600

    Started javadocs

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -1,3 +1,11 @@
+/*
+Copyright (c) 2016 Team 2, CMPUT 301, University of Alberta - All Rights Reserved.
+You may use, distribute and copy all or parts of this code under terms and conditions of
+University of Alberta and the Code of Student Behaviour.
+You can find the copy of license at http://www.github.com/Team20/...
+For further information, contact abc@abc.cas
+ */
+
 package ca.ualberta.cs.lonelytwitter;
 
 import java.io.BufferedReader;
@@ -22,18 +30,43 @@ import android.widget.ListView;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+/**
+ * This is the main view class of LonelyTwitter project. It handles all user interactions as well
+ * file manipulation.
+ * <prev>All the files are stored in the form of "json" files stores in Emulator accessible
+ * from Android device monitor.</prev>
+ * <code>
+ * Pseudo code sample: <br>
+ * open some file ... <br>
+ * attach some text... <br>
+ * load the file... <br>
+ * </code>
+ * <code>
+ * <ul>
+ * <item>item1</item>
+ * <item>item2</item>
+ * <item>item3</item>
+ * </ul>
+ * </code>
+ *
+ * @author joey  //@deprecated
+ * @see NormalTweet
+ * @since 1.0
+ */
 public class LonelyTwitterActivity extends Activity {
+    /**
+     * this is the filename that is being saved/loaded and contains all the tweets.
+     * @see #loadFromFile()
+     * @see #saveInFile()
+     */
 
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
-
 	private ArrayList<Tweet> tweetList = new ArrayList<Tweet>();
-
 	private ArrayAdapter<Tweet> adapter;
 
-	/** Called when the activity is first created. */
-	@Override
+    @Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
@@ -79,7 +112,12 @@ public class LonelyTwitterActivity extends Activity {
 		oldTweetsList.setAdapter(adapter);
 	}
 
-	private void loadFromFile() {
+    /**
+     * This method loads the tweets from FILENAME (file.sav), and ...
+     * @throws FileNotFoundException
+     * @exception RuntimeException
+     */
+    private void loadFromFile() {
 		try {
 			FileInputStream fis = openFileInput(FILENAME);
 			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
@@ -96,7 +134,12 @@ public class LonelyTwitterActivity extends Activity {
 			tweetList = new ArrayList<Tweet>();
 		}
 	}
-	
+
+    /**
+     * This method saves the tweets to FILENAME (file.sav), and...
+     * @throws FileNotFoundException
+     * @exception RuntimeException
+     */
 	private void saveInFile() {
 		try {
 			FileOutputStream fos = openFileOutput(FILENAME,
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -9,22 +9,45 @@ public abstract class Tweet {
     private String message;
     private Date date;
 
+    /**
+     * Instantiates a new Tweet (abstract). It saves the tweet text description as well as
+     * the creation date.
+     *
+     * @param message this parameter is the given string for the tweet. If it is set to empty string
+     *                then it will be replaced by...
+     * @param 
+     */
     public Tweet(String message){
         this.message = message;
         this.date = new Date();
     }
 
+    /**
+     * Instantiates a new Tweet.
+     *
+     * @param message the message
+     * @param date    the date
+     */
     public Tweet(String message, Date date){
         this.message = message;
         this.date = date;
     }
 
 
+    /**
+     * Is important boolean.
+     *
+     * @return the boolean
+     */
     public abstract Boolean isImportant();
 
 
-
-
+    /**
+     * Sets message.
+     *
+     * @param message the message
+     * @throws TweetTooLongException the tweet too long exception
+     */
     public void setMessage(String message) throws TweetTooLongException {
         if (message.length() > 140) {
             //Do something
@@ -33,14 +56,29 @@ public abstract class Tweet {
         this.message = message;
     }
 
+    /**
+     * Sets date.
+     *
+     * @param date the date
+     */
     public void setDate(Date date) {
         this.date = date;
     }
 
+    /**
+     * Gets message.
+     *
+     * @return the message
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Gets date.
+     *
+     * @return the date
+     */
     public Date getDate() {
         return date;
     }
